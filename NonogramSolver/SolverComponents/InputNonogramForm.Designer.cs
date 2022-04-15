f4521071943875a9daeebe66dfb3ccf777888661
Mythicaeda <62574120+Mythicaeda@users.noreply.github.com>
2022-04-15 13:44:42 -0400

    Uploaded Solver Components
    WIP on the Solving code, however visualization's all there. UserUpdatableListBox is a user control that's used on InputNonogram.

---
+++ NonogramSolver/SolverComponents/InputNonogramForm.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace NonogramSolverGenerator
+{
+    partial class InputNonogramForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.tTips = new System.Windows.Forms.ToolTip(this.components);
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.openNonDialog = new System.Windows.Forms.OpenFileDialog();
+            this.ulbCols = new NonogramSolverGenerator.SolverComponents.UserUpdatableListBox();
+            this.ulbRows = new NonogramSolverGenerator.SolverComponents.UserUpdatableListBox();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.Location = new System.Drawing.Point(44, 47);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(159, 38);
+            this.btnUpload.TabIndex = 1;
+            this.btnUpload.Text = "Upload";
+            this.tTips.SetToolTip(this.btnUpload, "Upload a preexisting .NONOGRAM file");
+            this.btnUpload.UseVisualStyleBackColor = true;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
+            // 
+            // btnBack
+            // 
+            this.btnBack.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnBack.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnBack.Location = new System.Drawing.Point(44, 394);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(159, 38);
+            this.btnBack.TabIndex = 0;
+            this.btnBack.Text = "Back";
+            this.tTips.SetToolTip(this.btnBack, "Return to Main Page");
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.BtnBack_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnNext.Location = new System.Drawing.Point(44, 321);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(159, 38);
+            this.btnNext.TabIndex = 3;
+            this.btnNext.Text = "Solve!";
+            this.tTips.SetToolTip(this.btnNext, "Return to Main Page");
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.panel2.BackColor = System.Drawing.Color.PowderBlue;
+            this.panel2.Controls.Add(this.btnNext);
+            this.panel2.Controls.Add(this.btnUpload);
+            this.panel2.Controls.Add(this.btnBack);
+            this.panel2.Location = new System.Drawing.Point(1, 1);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(246, 478);
+            this.panel2.TabIndex = 4;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(208)))), ((int)(((byte)(220)))));
+            this.panel1.Controls.Add(this.ulbCols);
+            this.panel1.Controls.Add(this.ulbRows);
+            this.panel1.Location = new System.Drawing.Point(245, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(602, 478);
+            this.panel1.TabIndex = 2;
+            // 
+            // ulbCols
+            // 
+            this.ulbCols.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.ulbCols.BtnAddToolTip = "";
+            this.ulbCols.BtnRemoveAllToolTip = "";
+            this.ulbCols.BtnRemoveToolTip = "";
+            this.ulbCols.Font = new System.Drawing.Font("Book Antiqua", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ulbCols.LabelToolTip = "";
+            this.ulbCols.ListBoxToolTip = "";
+            this.ulbCols.Location = new System.Drawing.Point(365, 24);
+            this.ulbCols.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.ulbCols.Name = "ulbCols";
+            this.ulbCols.Pattern = "^(\\b\\d+\\b\\s*)+$";
+            this.ulbCols.Size = new System.Drawing.Size(183, 431);
+            this.ulbCols.TabIndex = 9;
+            this.ulbCols.Text = "Columns:";
+            // 
+            // ulbRows
+            // 
+            this.ulbRows.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.ulbRows.BtnAddToolTip = "";
+            this.ulbRows.BtnRemoveAllToolTip = "";
+            this.ulbRows.BtnRemoveToolTip = "";
+            this.ulbRows.Font = new System.Drawing.Font("Book Antiqua", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ulbRows.LabelToolTip = "";
+            this.ulbRows.ListBoxToolTip = "";
+            this.ulbRows.Location = new System.Drawing.Point(55, 24);
+            this.ulbRows.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.ulbRows.Name = "ulbRows";
+            this.ulbRows.Pattern = "^(\\b\\d+\\b\\s*)+$";
+            this.ulbRows.Size = new System.Drawing.Size(183, 431);
+            this.ulbRows.TabIndex = 8;
+            this.ulbRows.Text = "Rows:";
+            // 
+            // InputNonogramForm
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 28F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PowderBlue;
+            this.CancelButton = this.btnBack;
+            this.ClientSize = new System.Drawing.Size(848, 480);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Font = new System.Drawing.Font("Book Antiqua", 18F);
+            this.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.Name = "InputNonogramForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Input Nonogram";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.InputNonogramForm_FormClosed);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.INForm_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.INForm_DragEnter);
+            this.panel2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnUpload;
+        private System.Windows.Forms.ToolTip tTips;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Panel panel2;
+        private SolverComponents.UserUpdatableListBox ulbRows;
+        private SolverComponents.UserUpdatableListBox ulbCols;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.OpenFileDialog openNonDialog;
+    }
+}
---
+++ NonogramSolver/SolverComponents/UserUpdatableListBox.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace NonogramSolverGenerator.SolverComponents
+{
+    public partial class UserUpdatableListBox : UserControl
+    {
+        //public event EventHandler EnterPressed;
+        //string pattern = @"(\b\d+\b\s*)+";
+        [Description("The Regex pattern used to validate new entries to the ListBox."),Category("Data")] 
+        public string Pattern { get; set; }
+
+        //adding ToolTip compatibility to all the controls
+        [Description("Determines the ToolTip shown when the mouse hovers over the Add Button.")]
+        public string BtnAddToolTip
+        {
+            get
+            {
+                return toolTip.GetToolTip(btnAdd);
+            }
+            set
+            {
+                toolTip.SetToolTip(btnAdd, value);
+            }
+        }
+        [Description("Determines the ToolTip shown when the mouse hovers over the Remove Button.")]
+        public string BtnRemoveToolTip
+        {
+            get
+            {
+                return toolTip.GetToolTip(btnRemove);
+            }
+            set
+            {
+                toolTip.SetToolTip(btnRemove, value);
+            }
+        }
+        [Description("Determines the ToolTip shown when the mouse hovers over the RemoveAll Button.")]
+        public string BtnRemoveAllToolTip
+        {
+            get
+            {
+                return toolTip.GetToolTip(btnRemoveAll);
+            }
+            set
+            {
+
+                toolTip.SetToolTip(btnRemoveAll, value);
+            }
+        }
+        [Description("Determines the ToolTip shown when the mouse hovers over the ListBox.")]
+        public string ListBoxToolTip
+        {
+            get
+            {
+                return toolTip.GetToolTip(listBox);
+            }
+            set
+            {
+
+                toolTip.SetToolTip(listBox, value);
+            }
+        }
+        [Description("Determines the ToolTip shown when the mouse hovers over the Name Label.")]
+        public string LabelToolTip
+        {
+            get
+            {
+                return toolTip.GetToolTip(lblName);
+            }
+            set
+            {
+
+                toolTip.SetToolTip(lblName, value);
+            }
+        }
+
+        //properties
+        [EditorBrowsable(EditorBrowsableState.Always)]
+        [Browsable(true)]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
+        [Bindable(true)]
+        public override string Text { get { return lblName.Text; } set { lblName.Text = value; } }
+        public ListBox ListBox { get { return listBox; } }
+        public Button RemoveAllButtom { get { return btnRemoveAll; } }
+
+
+        public UserUpdatableListBox()
+        {
+            InitializeComponent();
+            Pattern = @"^(\b\d+\b\s*)+$";
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (listBox.Items.Count < 1000)
+            {
+                listBox.Items.Add(tbNewEntry.Text.Trim());
+                tbNewEntry.Text = "";
+                //we have at least one item now
+                if (!btnRemoveAll.Enabled) btnRemoveAll.Enabled = true;
+            }
+            else
+            {
+                MessageBox.Show("Too many items in List.", "Can't Add Item", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                tbNewEntry.Text = "";
+            }
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            //remove all the items currently selected
+            for (int i = listBox.SelectedIndices.Count - 1; i >= 0; i--)
+            {
+                listBox.Items.RemoveAt(listBox.SelectedIndices[i]);
+            }
+            //if there are no more items, 
+            if (listBox.Items.Count == 0) btnRemoveAll.Enabled = false;
+        }
+
+        private void btnRemoveAll_Click(object sender, EventArgs e)
+        {
+            listBox.Items.Clear();
+            btnRemoveAll.Enabled = false;
+        }
+
+        private void tbNewEntry_TextChanged(object sender, EventArgs e)
+        {
+            if (Regex.Matches(tbNewEntry.Text, Pattern).Count == 1)
+            {
+                btnAdd.Enabled = true;
+            }   
+            else
+            {
+                btnAdd.Enabled = false;
+            }
+        }
+
+        private void tbNewEntry_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter && btnAdd.Enabled)
+            {
+                btnAdd_Click(this, e);
+            }
+        }
+
+        //this method is raised whenever an item is selected or deselected in multiselect mode
+        private void listBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(listBox.SelectedIndices.Count >= 1)
+            {
+                btnRemove.Enabled = true;
+            }
+            else
+            {
+                btnRemove.Enabled = false;
+            }
+        }
+
+        private void listBox_Leave(object sender, EventArgs e)
+        {
+            if (!btnRemove.Focused)
+            {
+                listBox.SelectedIndices.Clear();
+            }
+        }
+    }
+}
